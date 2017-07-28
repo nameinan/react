987d11987f342e2009f661cac2c4268ca6aac65c
nameinan <nandailogic@gmail.com>
2017-07-28 10:42:50 +0530

    Add jsx example

---
+++ 03/build/helloworld.js
@@ -0,0 +1,14 @@
+ var HelloWorld =React.createClass({displayName: "HelloWorld",
+
+
+        	 render: function(){
+                      return React.createElement("div", null, 
+                      			React.createElement("h1", null, "Hello world"), 
+                      			React.createElement("p", null, "This is some text")
+                     		 )
+        	 }
+
+
+        });
+
+        React.render(React.createElement(HelloWorld, null), document.body);
---
+++ 03/readme.txt
@@ -0,0 +1,4 @@
+Install react-tools  globally form npm
+Run  below command from npm terminal
+
+jsx /src /build
