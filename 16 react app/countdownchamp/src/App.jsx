5dd0b2f96119abc3bb4390801c8b4281676effa4
nameinan <nandailogic@gmail.com>
2017-08-03 15:46:46 +0530

    Add clock component

--- 16 react app/countdownchamp/src/App.jsx
+++ 16 react app/countdownchamp/src/App.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
+import Clock from './Clock';
 
 class App extends Component{
 
@@ -20,12 +21,7 @@ class App extends Component{
 
       <div className="App">
         <div className="App-title">Count down to {this.state.deadline}</div>
-        <div>
-          <div className="Clock-days">14 days</div>
-          <div className="Clock-hours">30 hours</div>
-          <div className="Clock-minutes">15 minutes</div>
-          <div className="Clock-seconds">20 seconds</div>
-        </div>
+        <Clock/>
         <div>
           <input placeholder="new date" onChange={ event=> this.setState({ newDeadline:event.target.value}) }/>
           <button onClick={()=>this.changeDeadLine()}>Submit</button>
